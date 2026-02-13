169b4515e3c7d4388bb6593ec40ea394b48027b7
mekal <mekal@GDQVP7CPCVN00.lan>
2026-02-13 12:44:11 +0000

    Just added comment

--- MyHitProject/src/testcases/TestCase1.java
+++ MyHitProject/src/testcases/TestCase1.java
@@ -6,8 +6,10 @@ public class TestCase1
 	public static void main(String[] args) 
 	{
 
-		System.out.println(" This is my First Test Case in Github -- Step 1 ");
-		System.out.println("  Client Updated  -- Step2  ");
+	System.out.println("  Just Updated  at Client Mechine Client Updated  Step 1  ");
+	System.out.println("  Just Updated  at Client Mechine Client Updated   Step2 ");
+	System.out.println("  Just Updated  Again   Step3 ");
+	
 	}
 
 }
