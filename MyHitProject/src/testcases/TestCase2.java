a223847c3a98d78a8b16db725c5c231a444eb61f
jaganmekala-cmd <jagenmekala@gmail.com>
2026-02-13 12:30:36 +0000

    Update TestCase2.java

--- MyHitProject/src/testcases/TestCase2.java
+++ MyHitProject/src/testcases/TestCase2.java
@@ -6,7 +6,8 @@ public class TestCase2
 	public static void main(String[] args) 
 	{
 	System.out.println(" Github Program from JAGAN Local Machine  ");
-
+    System.out.println(" Github Program from JAGAN Remote Machine  ");
 	}
 
 }
+
